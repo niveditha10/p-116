58dcf472e70fa64ef0647e741dacccf48b9ed31b
niveditha10 <73229406+niveditha10@users.noreply.github.com>
2021-03-29 17:11:20 +0530

    Add files via upload

---
+++ lipstick.js
@@ -0,0 +1,39 @@
+
+nose_x=0;
+nose_y=0;
+
+function preload() {
+nose=loadImage("https://i.postimg.cc/TwDRJvnZ/lipstick.png");
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.position(500,230)
+  video=createCapture(VIDEO);
+  video.size(300,300);
+  video.hide();
+
+posenet=ml5.poseNet(video,modelLoaded);
+posenet.on('pose',gotPoses);
+}
+function modelLoaded()
+{
+  console.log("posenet loaded");
+}
+
+function gotPoses(results){
+  if(results.length>0){
+    console.log(results);
+    nose_x=results[0].pose.nose.x-20;
+    nose_y=results[0].pose.nose.y+20;
+  }
+}
+
+function draw() {
+image(video,0,0,300,300);
+image(nose,nose_x,nose_y,50,30);
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png');
+}
